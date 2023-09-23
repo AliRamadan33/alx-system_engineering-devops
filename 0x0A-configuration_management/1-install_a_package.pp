853d76db386e958c6a6132a097e71f3075ad5822
LinCu0 <alitoba335@gmail.com>
2023-09-23 12:44:46 +0000

    Kill

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,5 @@
-# install puppet-lint -v 2.1.0
-
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  creates => '/usr/local/lib/python3.8/dist-packages/flask',
-  require => Package['python3-pip'],
+# installing a package using puppet
+package { 'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem',
 }
